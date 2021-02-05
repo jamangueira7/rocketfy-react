cc6d18be95d6e48ab071bca68a53e9659d630f4f
João Mangueira <jamjs7@gmail.com>
2021-02-05 18:40:26 -0300

    Primeiro visual do component List

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import List from '../List';
+
 import { Container } from './styles';
 
 export default function Board() {
     return (
         <Container>
-            <h1>Rocktfy</h1>
+            <List />
+            <List />
+            <List />
+            <List />
         </Container>
     );
 }
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import { MdAdd } from 'react-icons/md';
+
 import { Container } from './styles';
 
 export default function List() {
     return (
         <Container>
-            <h1>Rocktfy</h1>
+            <header>
+                <h2>Tarefas</h2>
+                <button type="button">
+                    <MdAdd size={24} color="#FFF"></MdAdd>
+                </button>
+            </header>
         </Container>
     );
 }
