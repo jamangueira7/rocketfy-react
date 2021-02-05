4889f6333aee2ebb580b3805bd57007fea81832f
João Mangueira <jamjs7@gmail.com>
2021-02-05 20:19:13 -0300

    Primeiros calculos para fazer o card mudar de posição dentro da propria lista

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import { useDrag } from 'react-dnd';
+import React, { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
 
 import { Container, Label } from './styles';
 
-export default function Card({ data }) {
+export default function Card({ data, index }) {
+    const ref = useRef();
+
     const [{ isDragging }, dragRef] = useDrag({
-       item:{ type: 'CARD'},
+       item:{ type: 'CARD', index },
        collect: monitor => ({
            isDragging: monitor.isDragging(),
        }),
     });
+
+    const [,dropRef] = useDrop({
+        accept: 'CARD',
+        hover(item, monitor) {
+            const draggedIndex = item.index;
+            const targetIndex = index;
+
+            if(draggedIndex === targetIndex) {
+                return;
+            }
+
+            //Calculando o meio do card
+            const targetSize = ref.current.getBoundingClientRect();
+            const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+
+            //Calculando a distancia do card para o topo da tela
+            const draggedOffset = monitor.getClientOffset();
+            const draggedTop = draggedOffset.y - targetSize.top;
+
+            //Evitando que o seja necessario fazer uma ação ate o card atingir o meio do card de baixo.
+            if(draggedIndex < targetIndex && draggedTop < targetCenter) {
+                return;
+            }
+            //Evitando que o seja necessario fazer uma ação ate o card atingin o meio do card de de cima.
+            if(draggedIndex > targetIndex && draggedTop > targetCenter) {
+                return;
+            }
+
+
+        }
+    });
+
+    dragRef(dropRef(ref));
+
     return (
-        <Container ref={dragRef} isDragging={isDragging}>
+        <Container ref={ref} isDragging={isDragging}>
             <header>
                 {data.labels.map(label => <Label key={label} color={label} />)}
             </header>
