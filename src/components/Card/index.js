9b2c7ffff0a010b5a77d7aeba4505cd9b2de5cbb
João Mangueira <jamjs7@gmail.com>
2021-02-05 19:12:02 -0300

    Conteudo dos Cards diniamico

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import { Container, Label } from './styles';
 
-export default function Card() {
+export default function Card({ data }) {
     return (
         <Container>
             <header>
-                <Label color="#7159c1" />
+                {data.labels.map(label => <Label key={label} color={label} />)}
             </header>
-            <p>Fazer a migração completa de servidor</p>
-            <img src="https://avatars.githubusercontent.com/u/1902749?s=460&u=382acdf7478c6499d9b4321e68523d17b699e802&v=4" alt="" />
+            <p>{data.content}</p>
+            {data.user && <img src={data.user} alt=""/>}
         </Container>
     );
 }
